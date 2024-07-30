cabf24a32b1f759413680ad377da4817d03d3b09
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-30 12:04:47 +0200

    v

--- app/Http/Controllers/api/v1/auth/LoginController.php
+++ app/Http/Controllers/api/v1/auth/LoginController.php
@@ -45,38 +45,6 @@ class LoginController extends Controller
         
     }
 
-    // Sign up Page
-
-    public function  signup(){
-        $countries = country::all();
-        $cities = city::all();
-        $categories = category::
-        where('category_id', '!=', null)
-        ->get();
-
-        return response()->json([
-            'countries' => $countries,
-            'cities' => $cities,
-            'categories' => $categories,
-        ]);
-    }
-
-    // Sign up Student Proccessing
-
-    public function signup_proccess(SignupRequest $request){
-        $requestSignup['type'] = 'student';
-        $data= $request->only($this->requestSignup);
-        User::create($data);
-
-        $token = $user->createToken('personal access Tokens')->plainTextToken;
-        return response()->json([
-            'success'=>'Sign Up Successfully',
-            '_token'=>$token,
-            'detailes'=>$data,
-        ]);
-        
-    }
-
     public function logout(Request $request)
     {
     if($request->user()){
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\api\v1\auth\LoginController;
+use App\Http\Controllers\api\v1\auth\SignupController;
+
 use App\Http\Controllers\api\v1\admin\ProfileAdminController;
 
 use App\Http\Controllers\api\v1\student\SubjectsController;
@@ -22,10 +25,13 @@ Route::middleware([StudentMiddleware::class, 'auth.sanctum'])->prefix('Student')
 });
 
 Route::controller(LoginController::class)->prefix('admin')->group(function () {
-    Route::post('auth/login','login')->name('login');
-    Route::get('auth/signup','signup')->name('signup');
-    Route::post('auth/signup_proccess','signup_proccess')->name('signup_proccess');
+    Route::post('auth/login','login')->name('login'); 
     Route::post('auth/logout','logout')->name('user.logout')->middleware('auth:sanctum');
+});
+
+Route::controller(SignupController::class)->prefix('admin')->group(function () { 
+    Route::get('auth/signup','signup')->name('signup');
+    Route::post('auth/signup_proccess','signup_proccess')->name('signup_proccess'); 
 });
 Route::controller(ProfileAdminController::class)->prefix('admin')->group(function () {
     Route::get('profile/view','view')->name('profile.admin')->middleware('auth:sanctum');
