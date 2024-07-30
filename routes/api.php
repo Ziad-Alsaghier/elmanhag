0d428acaf768d28786a0888545e6a4aa161954dd
Ahmed <ahmedahmadahmid73@gmail.com>
2024-07-30 09:03:42 +0200

    v

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\v1\auth\LoginController;
 use App\Http\Controllers\api\v1\admin\ProfileAdminController;
 
+use App\Http\Controllers\api\v1\student\SubjectsController;
+
+use App\Http\Middleware\StudentMiddleware;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+Route::middleware([StudentMiddleware::class, 'auth.sanctum'])->prefix('Student')->group(function () {
+    Route::controller(SubjectsController::class)->prefix('Subjects')->group(function () {
+        Route::get('/','Subjects')->name('Subjects');
+    });
+});
 
 Route::controller(LoginController::class)->prefix('admin')->group(function () {
     Route::post('auth/login','login')->name('login');
