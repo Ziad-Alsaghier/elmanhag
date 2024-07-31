f30e0fc273eb50a1add824e8387d21d223e775f5
Ziad <wegostationdev@gmail.com>
2024-07-31 08:40:49 +0200

    End APIs For Admin For Create New Student

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,36 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\StudentMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\StudentMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
+       then: function () {
+             Route::middleware('api')
+            ->prefix('admin')
+            ->name('admin.')
+            ->group(base_path('routes/admin.php'));
+       },
+        )
+    
     ->withMiddleware(function (Middleware $middleware) {
        [
-        'student' => $middleware->append(StudentMiddleware::class)
+        'student' => $middleware->append(StudentMiddleware::class),
+        'admin' => [
+                    'student'=>$middleware->append(AdminMiddleware::class)
+                    ]
         ];
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        
     })->create();
