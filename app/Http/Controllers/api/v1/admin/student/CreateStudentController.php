fd5a97c137d3d1fe8e000ccbac51f3e7e56f1041
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-03 10:27:48 +0200

    make request to update student

--- app/Http/Controllers/api/v1/admin/student/CreateStudentController.php
+++ app/Http/Controllers/api/v1/admin/student/CreateStudentController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\api\admin\student\StudentRequest;
+use App\Http\Requests\api\admin\student\UpdateStudentRequest;
 
 class CreateStudentController extends Controller
 {
@@ -33,18 +34,18 @@ class CreateStudentController extends Controller
     use image;
     public function store(StudentRequest $request){
         $newStudent =  $request->only($this->studentRequest); // Take only Request From Protected studentRequest names 
-       $image =  $this->upload($request,'image','admin/student'); // Start Upload Image
+        $image =  $this->upload($request,'image','admin/student'); // Start Upload Image
         $newStudent['type'] = 'student'; // Type Of User
         $newStudent['image'] =$image; // Image Value From traid Image 
        $user = $this->user->create($newStudent); // Start Create New Studetn
         return response()->json(['success'=>'Student Created Successfully'],200); 
     }
     
-    public function modify(StudentRequest $request, $id){
+    public function modify(UpdateStudentRequest $request, $id){
         $student =  $request->only($this->studentRequest); // Take only Request From Protected studentRequest names 
-       $image =  $this->upload($request,'image','admin/student'); // Start Upload Image
+        $image =  $this->upload($request,'image','admin/student'); // Start Upload Image
         $student['image'] =$image; // Image Value From traid Image 
-       User::where('id', $id)->update($student); // Start Create New Studetn
+        User::where('id', $id)->update($student); // Start Create New Studetn
         return response()->json(['success'=>'Student Updated Successfully'],200); 
     }
    
--- app/trait/image.php
+++ app/trait/image.php
@@ -7,12 +7,12 @@ trait image
 {
     // This Trait Aboute Image
 
-         public function upload(Request $request,$fileName = 'image',$directory){
-         if($request->has($fileName)){// if Request has a Image
-         $uploadImage = new request();
-         $imagePath = $request->file($fileName)->store($directory,'public'); // Take Image from Request And Save inStorage;
-         return $imagePath;
-         }
-         return Null;
-         }
+    public function upload(Request $request,$fileName = 'image',$directory){
+    if($request->has($fileName)){// if Request has a Image
+        $uploadImage = new request();
+        $imagePath = $request->file($fileName)->store($directory,'public'); // Take Image from Request And Save inStorage;
+        return $imagePath;
+    }
+    return Null;
+    }
 }
