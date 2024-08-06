7961f7434579565b28a1ab69733e54e6ca5aec06
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-06 11:32:05 +0200

    add functions to models

--- app/Models/subject.php
+++ app/Models/subject.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\User;
+use App\Models\Discount;
+use App\Models\category;
+use App\Models\chapter;
 
 class subject extends Model
 {
@@ -27,6 +30,22 @@ class subject extends Model
     ];
 
     public function users(){
-        return belongsToMany(User::class, 'students_subjects');
+        return $this->belongsToMany(User::class, 'students_subjects');
+    }
+
+    public function category(){
+        return $this->belongsTo(category::class);
+    }
+
+    public function chapters(){
+        return $this->hasMany(chapter::class);
+    }
+
+    public function discount(){
+        return $this->hasMany(Discount::class)
+        ->where('start_date', '<=', now())
+        ->where('end_date', '>=', now())
+        ->where('statue', 1)
+        ->orderByDesc('id');
     }
 }
