b3b20cc532749026ff772d521e9980f16af13083
Ahmed <ahmedahmadahmid73@gmail.com>
2024-08-06 14:12:22 +0200

    structure of create subject

--- app/Http/Controllers/api/v1/admin/Subject/CreateSubjectController.php
+++ app/Http/Controllers/api/v1/admin/Subject/CreateSubjectController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers\api\v1\admin\Subject;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\subject;
+
 class CreateSubjectController extends Controller
 {
-    //
+    public function add(){
+
+    }
 }
